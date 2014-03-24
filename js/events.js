57f0f5755e6cc839c10053bbe37d4add3aa0185c
Buymeapie <buymeapie@buymeapie.com>
2014-03-25 00:18:39 +0400

    end mqin work

---
+++ js/events.js
@@ -0,0 +1,110 @@
+$(document).ready(function() {
+
+  resizeWindow()
+  $(window).resize(function() {
+    resizeWindow();
+  });
+
+  $(document).on('mouseenter', 'div.small-point', function() {
+    $(this).closest('div.points-list').prev()
+      .find('div.msg-block-text')
+      .text($(this).find('div.point-msg').text())
+    
+    $(this).find('div.point-point').addClass('animated pulse')
+      .prev().addClass('animated border_pulse')
+      .prev().show();
+    $(this).closest('div.time-line-part').find('div.msg-block').show();
+  });
+
+  $(document).on('mouseleave', 'div.small-point', function() {
+    $(this).find('div.point-point').removeClass('animated pulse')
+      .prev().removeClass('animated border_pulse')
+      .prev().hide();
+    $(this).closest('div.time-line-part').find('div.msg-block').hide();
+  });
+
+  var jcarousel = $('.jcarousel');
+
+  jcarousel.on('jcarousel:reload jcarousel:create', function () {
+    var width_size_block = 350;
+    var width_last_size_block = 80;
+    var width_carousel_block = $('div.time-line-block').innerWidth();
+    var count_full_blocks = Math.floor((width_carousel_block - width_last_size_block) / width_size_block);
+    
+    $('div.jcarousel-wrapper')
+      .css('width', (count_full_blocks * width_size_block) + width_last_size_block + "px");
+    jcarousel.jcarousel('items')
+      .css('width', width_size_block + 'px');
+  })
+  .jcarousel();
+
+  $('.jcarousel-control-prev')
+    .on('jcarouselcontrol:active', function() {
+      $(this).removeClass('inactive');
+    })
+    .on('jcarouselcontrol:inactive', function() {
+      $(this).addClass('inactive');
+    })
+    .jcarouselControl({
+      target: '-=1'
+    });
+
+  $('.jcarousel-control-next')
+    .on('jcarouselcontrol:active', function() {
+      $(this).removeClass('inactive');
+    })
+    .on('jcarouselcontrol:inactive', function() {
+      $(this).addClass('inactive');
+    })
+    .jcarouselControl({
+        target: '+=1'
+    });
+
+  $('.jcarousel').on('jcarousel:animateend', function(event, carousel) {
+    if (lastItemIsVisible()) {
+      $('.jcarousel-control-next')
+        .addClass('inactive')
+        .jcarouselControl({  target: '-=0' });
+    } else {
+      $('.jcarousel-control-next')
+        .removeClass('inactive')
+        .jcarouselControl({  target: '+=1' });
+    }
+  });
+
+  $(document).on('mouseup', 'a.jcarousel-control-next,a.jcarousel-control-prev', function(event) {
+    if (!$(this).hasClass('inactive')) {
+      var target = $(this).hasClass('jcarousel-control-next') ? "-=1" : "+=1";
+      goParalax(target);
+    }
+  });
+});
+
+
+
+function lastItemIsVisible() {
+  var last_item = $('.jcarousel').jcarousel('items').last().get(0);
+  var result = false
+
+  _.map($('.jcarousel').jcarousel('visible'), function(item) {
+    if (item === last_item) {
+      result = true;
+    }
+  });
+
+  return result;
+}
+
+function resizeWindow() {
+  var padding_top_for_carcas = ($(window).height() - $("#carcas").height()) / 2;
+  $('#carcas').css('marginTop', padding_top_for_carcas + "px");
+}
+
+function goParalax(target) {
+  console.log(target)
+  $('div.background-paralax').animate({
+    left: (target === "+=1") ? "+=100" : "-=100",
+  }, 400, function() {
+    // Animation complete.
+  });
+}
--- js/main.js
+++ js/main.js
@@ -1,58 +1,237 @@
-//$("div.query-form div.input-text input").placeholder();
 
-$(document).ready(function() {
-  setInterval(function() {
-    var colors = [
-      '#000',
-      '#cdde54',
-      '#10b596',
-      '#516fb6',
-      '#f5966a',
-      '#9154a2',
-      '#de3c41'
-    ];
+var data = {
+  "1382043600":"Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts...",
+  "1382119200":"Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean...",
+  "1382086800":"A small river named Duden flows by their place and supplies it with the necessary regelialia...",
+  "1382065200":"It is a paradisematic country, in which roasted parts of sentences fly into your mouth...",
+  "1382119200":"Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar...",
+  "1382090400":"The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen...",
+  "1382065200":"She packed her seven versalia, put her initial into the belt and made herself on the way...",
+  "1382065200":"When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane...",
+  "1383955200":"Pityful a rethoric question ran over her cheek, then she continued her way...",
+  "1384016400":"On her way she met a copy...",
+  "1383966000":"The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word ...",
+  "1383998400":"Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar...",
+  "1383980400":"The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen...",
+  "1383998400":"She packed her seven versalia, put her initial into the belt and made herself on the way...",
+  "1383940800":"When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane...",
+  "1383948000":"Pityful a rethoric question ran over her cheek, then she continued her way...",
+  "1383948000":"On her way she met a copy...",
+  "1383944400":"The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word and and the Little Blind Text should turn around and return to its own, safe country...",
+  "1383984000":"But nothing the copy said could convince her and so it didn’t take long until a few insidious Copy Writers ambushed her, made her drunk with Longe and Parole and dragged her into their agency, where they abused her for their projects again and again...",
+  "1383940800":"And if she hasn’t been rewritten, then they are still using her...",
+  "1383944400":"Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts...",
+  "1383984000":"Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean...",
+  "1384009200":"A small river named Duden flows by their place and supplies it with the necessary regelialia...",
+  "1387054800":"It is a paradisematic country, in which roasted parts of sentences fly into your mouth...",
+  "1387101600":"Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar...",
+  "1387105200":"The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen...",
+  "1387126800":"She packed her seven versalia, put her initial into the belt and made herself on the way...",
+  "1387051200":"When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane...",
+  "1387116000":"Pityful a rethoric question ran over her cheek, then she continued her way...",
+  "1387130400":"On her way she met a copy...",
+  "1387065600":"The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word and and the Little Blind Text should turn around and return to its own, safe country...",
+  "1387083600":"But nothing the copy said could convince her and so it didn’t take long until a few insidious Copy Writers ambushed her, made her drunk with Longe and Parole and dragged her into their agency, where they abused her for their projects again and again...",
+  "1387130400":"And if she hasn’t been rewritten, then they are still using her...",
+  "1387065600":"Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts...",
+  "1387098000":"Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean...",
+  "1387076400":"A small river named Duden flows by their place and supplies it with the necessary regelialia...",
+  "1387069200":"It is a paradisematic country, in which roasted parts of sentences fly into your mouth...",
+  "1387069200":"Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar...",
+  "1387134000":"The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen...",
+  "1387069200":"She packed her seven versalia, put her initial into the belt and made herself on the way...",
+  "1387105200":"When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane...",
+  "1387076400":"Pityful a rethoric question ran over her cheek, then she continued her way...",
+  "1389132000":"On her way she met a copy...",
+  "1389207600":"The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word and and the Little Blind Text should turn around and return to its own, safe country...",
+  "1389146400":"But nothing the copy said could convince her and so it didn’t take long until a few insidious Copy Writers ambushed her, made her drunk with Longe and Parole and dragged her into their agency, where they abused her for their projects again and again...",
+  "1389186000":"And if she hasn’t been rewritten, then they are still using her...",
+  "1389189600":"Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts...",
+  "1392181200":"Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean...",
+  "1392220800":"It is a paradisematic country, in which roasted parts of sentences fly into your mouth...",
+  "1392195600":"Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar...",
+  "1392166800":"The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen...",
+  "1392206400":"Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar...",
+  "1392152400":"The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen...",
+  "1392156000":"She packed her seven versalia, put her initial into the belt and made herself on the way...",
+  "1392181200":"When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane...",
+  "1392228000":"Pityful a rethoric question ran over her cheek, then she continued her way...",
+  "1392192000":"The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word and and the Little Blind Text should turn around and return to its own, safe country...",
+  "1392231600":"But nothing the copy said could convince her and so it didn’t take long until a few insidious Copy Writers ambushed her, made her drunk with Longe and Parole and dragged her into their agency, where they abused her for their projects again and again...",
+  "1392217200":"And if she hasn’t been rewritten, then they are still using her...",
+  "1392181200":"Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts...",
+  "1392217200":"Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean...",
+  "1392170400":"A small river named Duden flows by their place and supplies it with the necessary regelialia...",
+  "1392163200":"It is a paradisematic country, in which roasted parts of sentences fly into your mouth...",
+  "1392170400":"Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar...",
+  "1392184800":"The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen...",
+  "1392217200":"She packed her seven versalia, put her initial into the belt and made herself on the way...",
+  "1392192000":"When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane...",
+  "1392202800":"Pityful a rethoric question ran over her cheek, then she continued her way...",
+  "1392152400":"On her way she met a copy..."
+}
 
-    $("div.logo div.title-container div.title").css('color', colors[Math.floor((Math.random()*6)+1)])
-  }, 5000);
 
+var Timeline = function(data) {
+  this.data = data;
+  this.dates_array = _.keys(this.data).sort();
+  this.big_point = false;
+  this.next_big_point = false;
+  this.current_li = false;
+  this.day_block_line_width = 261;
+  this.current_year = false;
+}
 
-  $("div.navigation-container div").on("click", function() {
-    var class_object = $(this).attr("class").slice(10);
+Timeline.prototype.run = function() {
+  var that = this
+  
+  _.map(that.dates_array, function(item) {
+    var current_item = {
+      date: new Date(item*1000),
+      text: that.data[item]
+    };
 
-    $('html, body').animate({
-          scrollTop: $("div." + class_object).offset().top
-    }, 500);
+    if (that.getNextBigPoint() &&
+      that.matchPostItems(current_item, that.getNextBigPoint())) {
+        that.setBigPoint(false);
+    }
+
+    if (!that.getBigPoint()) {
+      that.setBigPoint(current_item.date);
+      that.buildBigPoint(that.getBigPoint());
+      that.current_year = that.getBigPoint().getFullYear();
+
+      var next_day = that.findNextDayByBigPoint(that.getBigPoint())
+      that.setNextBigPoint(new Date(next_day*1000));
+
+      that.current_li = $('div.jcarousel ul li').last();
+    }
+
+    that.buildSmallPoint(current_item);
+
   });
 
+  /* create last today big point */
+  that.setBigPoint(new Date(that.getNextBigPoint()));
+  that.buildBigPoint(new Date(that.getNextBigPoint()));
+}
 
-  $("div.query-form form").on("submit", function() {
 
-    $("div.query-form div.error-name").hide();
+Timeline.prototype.matchPostItems = function(first_item, second_item) {
+  if (first_item.date.getTime() === second_item.getTime()) {
+    return true
+  }
+  return false;
+}
 
-    if ($.trim($("div.input-text input")[0].value) === "") {
-      $("div.query-form div.error-name").show();
-      return false;
-    }
 
-    if ($.trim($("div.input-text input")[1].value) === "") {
-      $("div.query-form div.error-phone").show();
-      return false;
-    }
+Timeline.prototype.buildBigPoint = function(big_point) {
+  var that = this;
+  $('div.jcarousel ul').append('<li>' +
+    '<div class="time-line-part">' +
+      '<div class="year">' + (that.current_year != that.getBigPoint().getFullYear() ? that.getBigPoint().getFullYear() : "") + '</div>' + 
+      '<div class="msg-block"><div></div>' +
+        '<div class="msg-block-text"></div>' +
+      '</div>' +
+      '<div class="points-list">' +
+        '<div class="point big-point">' +
+          '<p class="point-date">' + that.getBigPoint().getDate() + '</p>' +
+          '<p class="point-month">' + getRussianMonths()[that.getBigPoint().getMonth()] + '</p>' +
+        '</div>' +
+      '</div>' +
+    '</div>' +
+  '</li>');
+}
 
-    $.ajax({
-      url: "/info.php",
-      dataType: "json",
-      data: {
-        name: $.trim($("div.query-form div.input-text input")[0].value),
-        phone: $.trim($("div.query-form div.input-text input")[1].value)
-      },
-      type: "post",
-      success: function(data) {
-        if (data === "ok") {
-          $("div.query-form div.success").show();
-        }
+
+Timeline.prototype.getNextBigPoint = function(big_point) {
+  return this.next_big_point;
+}
+
+
+Timeline.prototype.setNextBigPoint = function(big_point) {
+  this.next_big_point = big_point;
+}
+
+
+Timeline.prototype.getBigPoint = function(big_point) {
+  return this.big_point;
+}
+
+
+Timeline.prototype.setBigPoint = function(big_point) {
+  this.big_point = big_point;
+}
+
+
+Timeline.prototype.buildSmallPoint = function(small_point) {
+  var that = this;
+  that.current_li.find('div.points-list')
+    .append('<div class="point small-point" style="left:' + that.getLeftPositionForPoint(small_point) + 'px;">' +
+      '<div class="point-msg-corner"></div>' +
+      '<div class="point-border"></div>' +
+      '<div class="point-point"></div>' +
+      '<div class="point-msg">' + small_point.text + '</div>' +
+    '</div>');
+}
+
+
+Timeline.prototype.findNextDayByBigPoint = function(big_point) {
+  var result = false;
+
+  _.map(this.dates_array, function(item) {
+    if (!result) {
+      var count_days_between_dates = Math.floor(((item*1000) - big_point.getTime()) / (1000*60*60*24))
+      if (count_days_between_dates > 1) {
+        result = item;
       }
-    })
-    return false;
+    }
   });
-});
+
+  if (!result) {
+    var now = new Date();
+    result = new Date(now.getFullYear(), now.getMonth(), now.getDate()).getTime() / 1000;
+  }
+  
+  return result;
+}
+
+
+Timeline.prototype.findLastTimeOfDayByBigPoint = function(big_point) {
+  var result = [];
+
+  _.map(this.dates_array, function(item) {
+    var count_days_between_dates = Math.floor(((item*1000) - big_point.getTime()) / (1000*60*60*24))
+    if (count_days_between_dates < 1) {
+      result.push(item);
+    }
+  });
+  
+  return result[result.length - 1];
+}
+
+
+Timeline.prototype.getLeftPositionForPoint = function(small_point) {
+  var last_time_of_big_date_group = this.findLastTimeOfDayByBigPoint(this.getBigPoint());
+  var differ_lst_time_bd_millsec = (last_time_of_big_date_group*1000) - this.getBigPoint().getTime();
+  var differ_lst_time_currdat_millsec = small_point.date.getTime() - this.getBigPoint().getTime();
+  var different_coeff = (differ_lst_time_currdat_millsec / differ_lst_time_bd_millsec) * 100;
+
+  return this.day_block_line_width / 100 * different_coeff;
+}
+
+
+
+
+
+
+
+
+function getRussianMonths() {
+  return ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+}
+
+
+
+var timeline= new Timeline(data);
+timeline.run();
